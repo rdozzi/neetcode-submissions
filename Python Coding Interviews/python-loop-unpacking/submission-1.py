4aa0e168be64aaa30a96a43e4f3ccd5995407243
Raphael Dozzi <rdozzi84@gmail.com>
2026-06-26 19:37:52 -0700

    Add: python-loop-unpacking - submission-1

---
+++ Python Coding Interviews/python-loop-unpacking/submission-1.py
@@ -0,0 +1,22 @@
+from typing import List, Tuple
+
+
+def best_student(scores: List[Tuple[str, int]]) -> str:
+    final_name = None
+    highest_score = -1
+
+    for name, score in scores:
+        if(score > highest_score):
+            # highest_score = score
+            # final_name = name
+            highest_score, final_name = score, name
+    
+    return final_name
+        
+
+
+# do not modify below this line
+print(best_student([("Alice", 90), ("Bob", 80), ("Charlie", 70)]))
+print(best_student([("Alice", 90), ("Bob", 80), ("Charlie", 100)]))
+print(best_student([("Alice", 90), ("Bob", 100), ("Charlie", 70)]))
+print(best_student([("Alice", 90), ("Bob", 90), ("Charlie", 80), ("David", 100)]))
