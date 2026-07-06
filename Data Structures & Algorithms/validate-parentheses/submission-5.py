d241fc587bbacf37e178c83f19ba32e2f930c3a6
Raphael Dozzi <rdozzi84@gmail.com>
2026-07-05 22:59:34 -0700

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.py
@@ -0,0 +1,31 @@
+# I can't think of how to obtain the brute force O(n^2) solution for this problem
+# Could you use two pointers for this instead of a stack? Create if/else statements
+#  to check compliments? No. 
+# The Brute Force method, searching and replacing pairs in O(n^2) time are the
+# and stack method are the only solutions to this problem
+# Add the left hand brackets to the stack and remove the 
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+
+        stack = []
+
+        for brk in s:
+            if(brk in '({['):
+                stack.append(brk)
+            elif(brk == ')' and stack and stack[-1] == '('):
+                stack.pop()
+            elif(brk == ']' and stack and stack[-1] == '['):
+                stack.pop()
+            elif(brk == '}' and stack and stack[-1] == '{'):
+                stack.pop()
+            else:
+                return False
+        
+        return not stack
+
+
+
+
+
+        
